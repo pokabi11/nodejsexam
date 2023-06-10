5cd96fbf81a1b75439f7c7606f3b5c866fcae2d5
Long Nguyễn Hoàng <hlong2k1@gmail.com>
2023-06-10 11:08:59 +0700

    a

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,28 +5,5 @@ router.get("/",controller.get);
 router.get("/create",controller.create);
 router.post("/create",controller.save);
 
-router.get("/",(req,res)=>{
-    User.find({})
-        .then(uf=>{
-            res.render("user/list",{
-                users: uf
-            })
-        })
-        .catch(err=>{
-            res.send(err);      
-        })
-});
-router.get("/create",(req,res)=>{
-    res.render("user/form");
-});
-router.post("/create",(req,res)=>{
-    const data = req.body;
-    const user = new User(data);
-    user.save()
-    .then(uf=>{
-        res.redirect("/users/");
-    }).catch(err=>{
-        res.send(err);
-    })
-});
+
 module.exports = router;
